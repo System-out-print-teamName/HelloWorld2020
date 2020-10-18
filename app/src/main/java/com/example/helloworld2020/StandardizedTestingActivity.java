26fca1418911e8344d5daf2d3603b2b1a77fb896
x-pham <xpham@purdue.edu>
2020-10-17 20:48:32 -0400

    Removed DarkModeActionBar from themes.xml and implemented a 10 second notification system.

--- app/src/main/java/com/example/helloworld2020/StandardizedTestingActivity.java
+++ app/src/main/java/com/example/helloworld2020/StandardizedTestingActivity.java
@@ -1,11 +1,19 @@
 package com.example.helloworld2020;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class StandardizedTestingActivity extends AppCompatActivity {
 
@@ -13,6 +21,8 @@ public class StandardizedTestingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standardized_testing);
+
+        createNotificationChannel();
 
         String nextSatTest = "August";
         Button nextSatBtn = (Button)findViewById(R.id.nextSatDateButton);
@@ -26,7 +36,8 @@ public class StandardizedTestingActivity extends AppCompatActivity {
         satPrepBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(StandardizedTestingActivity.this, SatPrepActivity.class));
+                startActivity(new Intent(StandardizedTestingActivity.this,
+                        SatPrepActivity.class));
             }
         });
 
@@ -34,8 +45,44 @@ public class StandardizedTestingActivity extends AppCompatActivity {
         actPrepBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(StandardizedTestingActivity.this, ActPrepActivity.class));
+                startActivity(new Intent(StandardizedTestingActivity.this,
+                        ActPrepActivity.class));
             }
         });
+
+        Button dailyAlarmButton = (Button)findViewById(R.id.dailyAlarm);
+
+        dailyAlarmButton.setOnClickListener(v -> {
+            Toast.makeText(this, "Reminder Set!", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(StandardizedTestingActivity.this,
+                    ReminderBroadcast.class);
+            PendingIntent pendingIntent =
+                    PendingIntent.getBroadcast(StandardizedTestingActivity.this,
+                            0, intent, 0);
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+            long timeAtButtonClick = System.currentTimeMillis();
+
+            long tenSecondsInMillis = 1000 * 10;
+
+            alarmManager.set(AlarmManager.RTC_WAKEUP, timeAtButtonClick +
+                    tenSecondsInMillis, pendingIntent);
+        });
+    }
+
+
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "LemubitReminderChannel";
+            String description = "Channel for Lemubit Reminder";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("notifyLemubit", name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
